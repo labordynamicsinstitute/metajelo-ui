73d6301134671de1d17e86cf57ba252a012c7c40
Brandon Elam Barker <beb82@cornell.edu>
2019-07-11 20:20:33 +0000

    retain values for form; reverted form to use submit button

--- app/src/Metajelo/Forms.purs
+++ app/src/Metajelo/Forms.purs
@@ -31,7 +31,7 @@ import Metajelo.Validation as V
 import Metajelo.View (contactWidg)
 import Metajelo.XPaths.Read as MR
 import Prim.Row (class Cons)
-import Text.Email.Validate (EmailAddress)
+import Text.Email.Validate (EmailAddress, toString)
 
 newtype InstContactForm r f = InstContactForm (r (
     email1 :: f V.FieldError String EmailAddress
@@ -49,11 +49,26 @@ type InputForm = InstContactForm Record F.InputField
 type Validators = InstContactForm Record (F.Validation InstContactForm (Widget HTML))
 type FState = F.State InstContactForm (Widget HTML)
 
-initialInputs :: InputForm
-initialInputs = F.wrapInputFields {
+
+type InputRecord = {
+  email1:: String
+, email2:: String
+, contactType :: Maybe M.InstitutionContactType
+}
+
+initialInputsRecord :: InputRecord
+initialInputsRecord = {
   email1: ""
 , email2: ""
 , contactType: Nothing
+}
+
+outToInRec ::  Maybe M.InstitutionContact -> InputRecord
+outToInRec Nothing = initialInputsRecord
+outToInRec (Just outRec) = let email = toString outRec.emailAddress in {
+  email1: email
+, email2: email
+, contactType: outRec.contactType
 }
 
 validators :: Validators
@@ -79,8 +94,8 @@ contactForm fstate = do
       , (F.asyncSetValidate debounceTime proxies.email2 <<< P.unsafeTargetValue) <$> P.onChange
       ]
     , errorDisplay $ F.getError proxies.email2 fstate.form
-    , D.div' [D.text "Contact type: ", F.submit <$ menu fstate.form proxies.contactType]
-    -- , D.div' [ F.submit <$ D.button [P.onClick] [D.text "Submit"]]
+    , D.div' [D.text "Contact type: ",  menu fstate.form proxies.contactType]
+    , D.div' [ F.submit <$ D.button [P.onClick] [D.text "Submit"]]
     ]
   res <- F.eval query fstate
   case res of
@@ -97,9 +112,10 @@ contactForm fstate = do
 contactSignal :: Maybe M.InstitutionContact
   -> Signal HTML (Maybe M.InstitutionContact)
 contactSignal instContactMay = step instContactMay do
+  inputs <- pure $ F.wrapInputFields $ outToInRec instContactMay
   instContact <- D.div' [
     D.h2' [D.text "Institution Contact"]
-  , contactForm (initState initialInputs validators)
+  , contactForm (initState inputs validators)
   , foldMap contactWidg instContactMay
   ]
   pure $ contactSignal $ Just instContact
