d8cca605465a645974f366892799b5e9211985f2
Brandon Barker <beb82@cornell.edu>
2019-07-11 21:51:11 +0000

    working on accumulate Record

--- app/src/Metajelo/UI.purs
+++ app/src/Metajelo/UI.purs
@@ -1,6 +1,6 @@
 module Metajelo.UI where
 
-import Prelude (Unit, bind, pure, unit, ($), (<>))
+import Prelude (Unit, bind, discard, pure, unit, ($), (<>))
 
 import Concur.Core (Widget)
 import Concur.Core.FRP (Signal, display, dyn, step)
@@ -14,14 +14,42 @@ import Metajelo.Forms as MF
 import Metajelo.Types as M
 
 
+-- TODO: extract this example as a library and use to accumulate values as below:
+-- TODO: https://github.com/natefaubion/purescript-heterogeneous/blob/master/test/Record.purs#L150-L19
+-- -- | Accumulator type for `MetajeloRecord`, with each field
+-- -- | wrapped in `Maybe`.
+-- type MetajeloRecordPartial = {
+  
+-- --   identifier :: Maybe Identifier
+-- -- , date :: Maybe XsdDate
+-- --   -- ^ The date of the original creation of this metadata record
+-- -- , lastModified :: Maybe XsdDate
+-- --   -- ^ The date of the most recent modification of this recocrd
+-- -- , relatedIdentifiers :: Maybe NonEmptyArray RelatedIdentifier
+-- -- , supplementaryProducts :: Maybe NonEmptyArray SupplementaryProduct
+-- --   -- ^ The link to the set of supplemenary products
+-- }
+
 main :: Effect Unit
 main = pure unit
 
 runFormSPA :: String -> Effect Unit
 runFormSPA divId = runWidgetInDom divId page
 
+-- TODO: so far just a test of retrieving data from signals
+accumulateRecord :: String -> Signal HTML String
+accumulateRecord str = step str do
+  ic :: Maybe M.InstitutionContact <- D.div' [
+    -- FIXME: dyn means this will never return
+    dyn $ MF.contactSignal Nothing
+  , D.text str
+  ]
+  let icString = show $ ic
+  let newStr = "Completed Data from forms:\n" <> icString
+  pure $ accumulateRecord str
+
 page :: forall a. Widget HTML a
 page = do
-  dyn $ MF.contactSignal Nothing
+  dyn $ accumulateRecord ""
 
 
