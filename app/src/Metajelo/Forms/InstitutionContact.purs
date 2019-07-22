919e62c82562f52127f7db76b02b97d72f0402f6
Brandon Elam Barker <beb82@cornell.edu>
2019-07-22 16:25:48 +0000

    converted form to a signal that returns its current state and query

--- app/src/Metajelo/Forms/InstitutionContact.purs
+++ app/src/Metajelo/Forms/InstitutionContact.purs
@@ -1,12 +1,13 @@
 module Metajelo.Forms.InstitutionContact where
 
-import Prelude (bind, pure, ($), (<$>), (<<<))
+import Prelude (Unit, bind, discard, pure, unit, ($), (<$>), (<<<))
 
 import Concur.Core (Widget)
-import Concur.Core.FRP (Signal, step)
+import Concur.Core.FRP (Signal, display, dyn, step)
 import Concur.React (HTML)
 import Concur.React.DOM as D
 import Concur.React.Props as P
+import Control.Alt ((<|>))
 import Control.Applicative ((<$))
 import Control.Category ((>>>))
 import Data.Either (Either(..))
@@ -37,9 +38,29 @@ type InputForm = InstContactForm Record F.InputField
 type Validators = InstContactForm Record (F.Validation InstContactForm (Widget HTML))
 type FState = F.State InstContactForm (Widget HTML)
 
+type QueryWithFState form r = {
+  query :: Maybe (F.Query form)
+, fstate :: FState
+, result :: Maybe r
+}
+
+qwf :: forall form r. F.Query form -> FState -> Maybe r -> QueryWithFState form r
+qwf query fstate res = {
+  query: Just query
+, fstate: fstate
+, result: res
+}
+
+
+qwfPureState :: forall form r. FState -> QueryWithFState form r
+qwfPureState st = {
+  query: Nothing
+, fstate: st
+, result: Nothing
+}
 
 type InputRecord = {
-  email1:: String
+  email1 :: String
 , contactType :: Maybe M.InstitutionContactType
 }
 
@@ -62,24 +83,27 @@ validators = InstContactForm {
 , contactType: V.dummy
 }
 
-contactForm :: FState -> Widget HTML M.InstitutionContact
-contactForm fstate = do
+
+-- TODO: make type synonym for this:
+contactForm :: QueryWithFState InstContactForm M.InstitutionContact -> Signal HTML (QueryWithFState InstContactForm M.InstitutionContact)
+contactForm qwFstate = step qwFstate do
   query <- D.div' [
-      D.div' [D.text "Email"]
-    , D.input [
-        P.value $ F.getInput proxies.email1 fstate.form
+    D.div' [D.text "Email"]
+  , D.input [
+        P.value $ F.getInput proxies.email1 qwFstate.fstate.form
       , (F.setValidate proxies.email1 <<< P.unsafeTargetValue) <$> P.onChange
-      ]
-    , errorDisplay $ F.getError proxies.email1 fstate.form
-    , D.div' [D.text "Contact type: ",  menu fstate.form proxies.contactType]
-    , D.div' [ F.submit <$ D.button [P.onClick] [D.text "Submit"]]
     ]
-  res <- F.eval query fstate
+  , errorDisplay $ F.getError proxies.email1 qwFstate.fstate.form
+  , D.div' [D.text "Contact type: ",  menu qwFstate.fstate.form proxies.contactType]
+  , D.div' [ F.submit <$ D.button [P.onClick] [D.text "Submit"]]
+  ]
+  res <- F.eval query qwFstate.fstate
   case res of
-    Left fstate' -> contactForm fstate'
+    Left fstate' -> pure $ contactForm $ qwf query fstate' Nothing
     Right out -> do
       let form = F.unwrapOutputFields out
-      pure {emailAddress: form.email1, contactType: form.contactType}
+      let ic = {emailAddress: form.email1, contactType: form.contactType}
+      pure $ contactForm $ qwf query qwFstate.fstate (Just ic)
   where
     errorDisplay = maybe mempty (\err ->
       D.div [P.style {color: "red"}] [D.text $ V.toText err]
@@ -89,14 +113,12 @@ contactForm fstate = do
 
 contactSignal :: Maybe M.InstitutionContact
   -> Signal HTML (Maybe M.InstitutionContact)
-contactSignal instContactMay = step instContactMay do
-  inputs <- pure $ F.wrapInputFields $ outToInRec instContactMay
-  instContact <- D.div' [
-    D.h2' [D.text "Institution Contact"]
-  , contactForm (initState inputs validators)
-  , foldMap contactWidg instContactMay
-  ]
-  pure $ contactSignal $ Just instContact
+contactSignal instContactMay = D.div_ [] do
+  let inputs = (F.wrapInputFields $ outToInRec instContactMay)
+  display $ D.h2' [D.text "Institution Contact"]
+  formData <- contactForm $ qwfPureState (initState inputs validators)
+  display $ foldMap contactWidg instContactMay
+  pure formData.result
 
 
 -- This should be in Formless
